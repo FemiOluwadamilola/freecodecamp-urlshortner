eaab11b20e83b8afe45b97879aa19e93abb98485
Femi Oluwadamilola <oluwadamilola1902@gmail.com>
2023-05-06 02:58:48 +0100

    add mongodb atlas connection string

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const db = process.env.DB_URI;
 mongoose
   .connect("mongodb://127.0.0.1:27017/urlshortner", {
     useNewUrlParser: true,
